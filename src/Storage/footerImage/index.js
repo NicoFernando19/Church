d9e3d33d959cb6df0d689da7156bd6eb75bc0a9c
NicoFernando19 <nicofernando39@gmail.com>
2022-06-29 23:51:28 +0700

    added footer component

---
+++ src/Storage/footerImage/index.js
@@ -0,0 +1,6 @@
+import Facebook from "./facebook.png";
+import Twitter from "./twitter.png";
+import Logo from "./logo.png";
+import LinkedIn from "./linkedin.png";
+
+export { Facebook, Twitter, Logo, LinkedIn };
---
+++ src/elements/text/Text.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Text.module.scss";
+
+const Text = ({ heading, children, className }) => {
+  return (
+    <span
+      className={`${styles.text} ${
+        heading
+          ? heading.includes("h1")
+            ? styles.heading1
+            : heading.includes("h2")
+            ? styles.heading2
+            : heading.includes("h3")
+            ? styles.heading3
+            : heading.includes("h4")
+            ? styles.heading4
+            : heading.includes("h5")
+            ? styles.heading5
+            : styles.heading6
+          : styles.defaultSize
+      }`}
+    >
+      <p className={`${className}`}>{children}</p>
+    </span>
+  );
+};
+
+export default Text;
