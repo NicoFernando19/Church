31245e3ebee04a6e971a0dac192ef63e3f8ce04f
NicoFernando19 <nicofernando39@gmail.com>
2022-07-08 18:07:45 +0700

    last rows

---
+++ src/Components/cardv2/CardV2.tsx
@@ -0,0 +1,40 @@
+import Text from 'elements/text/Text';
+import React from 'react';
+import styles from './CardV2.module.scss';
+
+interface Props {
+  title?: string;
+  desc?: string;
+  speaker: string;
+  date: string;
+}
+
+const defaultProps = {
+  title: 'WATCH AND LISTEN TO OUR SERMONS',
+  desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.',
+  speaker: 'By Mathew Johnson',
+  date: 'Tuesday 13 May, 2021',
+};
+
+const CardV2 = ({ title, desc, speaker, date }: Props) => {
+  return (
+    <div className={`${styles.card} ${styles.bgColor}`}>
+      <div className={styles.padding}>
+        <Text className={styles.relation}>Relationship</Text>
+        <Text heading={'h5'} className={styles.title}>
+          {title}
+        </Text>
+        <Text heading={'h6'} className={styles.desc}>
+          {desc}
+        </Text>
+        <Text className={styles.speaker}>{speaker}</Text>
+        <Text className={styles.date}>{date}</Text>
+      </div>
+      <div className={styles.cardFooter}></div>
+    </div>
+  );
+};
+
+CardV2.defaultProps = defaultProps;
+
+export default CardV2;
